f31675ec21963a82e5a74dbe6a909b62675e9a2b
Kairos22 <alandiko84@gmail.com>
2022-09-30 17:22:44 +0200

    Working on

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,29 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
 let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
+  window.setTimeout(function () {
+    resolve(modal);
+  }, 1000 * 60);
+});
+async function getModal() {
+  let result = await showModal();
+  result.style.display = "block";
+}
+
+getModal();
+
+promiseOfModal.then(function (val) {
+  console.log("User has been on the page for 60 seconds");
+  val.style.display = "block";
 });
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
-
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
-    }
-})
-
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
